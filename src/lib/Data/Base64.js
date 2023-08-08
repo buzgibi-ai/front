a95670ab572862b4a11a5f77f96fc7d853d8f33a
Sergey Yakovlev <fclaw007@gmail.com>
2023-08-08 12:17:05 +0300

    base64

--- src/lib/Data/Base64.js
+++ src/lib/Data/Base64.js
@@ -3,51 +3,51 @@
 
 function validateBase64(s) {
     if (!(/^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/.test(s))) {
-      throw new TypeError('invalid encoding');
+        throw new TypeError('invalid encoding');
     }
-  }
-  
-export const encodeBase64Impl = function (buffer) {
+}
+
+export const encodeBase64Impl = function(buffer) {
     if (typeof btoa === 'undefined') {
-      // Node
-      return Buffer.from(buffer).toString('base64');
+        // Node
+        return Buffer.from(buffer).toString('base64');
     } else {
-      // Browser
-      const reducer = function (a, n) {
-        return a.concat([String.fromCharCode(n)]);
-      };
-      const arr = new Array();
-      const wrapped = new Uint8Array(buffer);
-      return btoa(wrapped.reduce(reducer, arr).join(''));
-    }
-  };
-  
-export const decodeBase64Impl = function (just, nothing, str) {
-    try {
-      validateBase64(str);
-      if (typeof atob === 'undefined') {
-        arrayRepresentation = new Uint8Array(Array.prototype.slice.call(Buffer.from(str, 'base64'), 0));
-        return just(arrayRepresentation.buffer);
-      } else {
         // Browser
-        var i = 0;
-        const byteString = atob(str);
-        var ns = new Uint8Array(byteString.length);
-        for (i = 0; i < ns.length; i++) {
-          ns[i] = byteString.charCodeAt(i);
-        }
-        return just(ns.buffer);
-      }
-    } catch (e) {
-      return nothing;
+        const reducer = function(a, n) {
+            return a.concat([String.fromCharCode(n)]);
+        };
+        const arr = new Array();
+        const wrapped = new Uint8Array(buffer);
+        return btoa(wrapped.reduce(reducer, arr).join(''));
     }
-  };
-  
-export const fromStringImpl = function (just, nothing, str) {
+};
+
+export const decodeBase64Impl = function(just, nothing, str) {
     try {
-      validateBase64(str);
-      return just(str);
+        validateBase64(str);
+        if (typeof atob === 'undefined') {
+            arrayRepresentation = new Uint8Array(Array.prototype.slice.call(Buffer.from(str, 'base64'), 0));
+            return just(arrayRepresentation.buffer);
+        } else {
+            // Browser
+            var i = 0;
+            const byteString = atob(str);
+            var ns = new Uint8Array(byteString.length);
+            for (i = 0; i < ns.length; i++) {
+                ns[i] = byteString.charCodeAt(i);
+            }
+            return just(ns.buffer);
+        }
     } catch (e) {
-      return nothing;
+        return nothing;
     }
-  }
+};
+
+export const fromStringImpl = function(just, nothing, str) {
+    try {
+        validateBase64(str);
+        return just(str);
+    } catch (e) {
+        return nothing;
+    }
+}
